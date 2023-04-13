cdcd41413071fc6c24ecd5c14ce208c3cb9a6582
sanket wagh <sanketwagh147@gmail.com>
2023-04-14 01:28:27 +0530

    vid rentals

---
+++ my_video_rental/videos_project/videos/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+
+from . import models
+
+# Register your models here.
+
+
+admin.site.register(models.Customer)
+admin.site.register(models.Movie)
+
+
+# Register your models here.
